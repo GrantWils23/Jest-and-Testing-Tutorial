c3edfc7f45f2aacb640f1be43901908baf509694
Grant <GrantWils.23@googlemail.com>
2022-02-08 18:46:54 +0000

    add final logic to the jest tutorial

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,12 +1,40 @@
-const buttonClick = require("../button.");
+/**
+ * @jest-environment jsdom
+ */
+
+const buttonClick = require("../button");
+
+
+
+// SINGLE FILE MOCKING
+
+// beforeEach(() => {
+//     document.getElementById.innerHTML = '<p id="par"></p>'
+// })
+
+// describe("DOM tests", () => {
+//     test("expect p content to change", () => {
+//         buttonClick();
+//         expect(document.getElementById("par").innerHTML).toEqual("You Clicked")
+//     })
+// })
+
+// MOCK TESTING MORE THAN ONE DOM
 
 beforeEach(() => {
-    document.getElementById.innerHTML = '<p id="par"></p>'
-})
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
 
 describe("DOM tests", () => {
-    test("expect p content to change", () => {
+    test("expect p conent to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked")
-    })
-})
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toEqual(1);
+    });
+});
